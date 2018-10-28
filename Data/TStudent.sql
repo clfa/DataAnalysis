f352d23ac7393518a6aca096790c97b8bfe31961
clfa <liangfengchen@yahoo.com>
2018-10-28 22:41:19 +0800

    backup some files

---
+++ Data/TStudent.sql
@@ -0,0 +1,22 @@
+Create table T.TStudent(
+ID int PRIMARY KEY NOT NULL,
+NAME varchar2( 20 ) NOT NULL,
+AGE int );
+
+insert into T.TStudent (ID, NAME, AGE)
+values (1001, '程龙', 4);
+
+insert into T.TStudent (ID, NAME, AGE)
+values (1002, '李雷', 5);
+
+insert into T.TStudent (ID, NAME, AGE)
+values (1003, 'Lily', 4);
+
+insert into T.TStudent (ID, NAME, AGE)
+values (1004, 'Lucy', 4);
+
+insert into T.TStudent (ID, NAME, AGE)
+values (1005, '韩梅梅', 5);
+
+insert into T.TStudent (ID, NAME, AGE)
+values (1006, '弗兰克', 4);
